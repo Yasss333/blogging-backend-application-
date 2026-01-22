b0734741b2f3e8ab877b2aedd52e572fe6ce5a44
Yasss333 <yashmandhare3@gmail.com>
2026-01-22 22:35:23 +0530

    fixed isuues

--- app.js
+++ app.js
@@ -21,16 +21,39 @@ app.use(express.static("public"));
 
 app.use("/uploads", express.static("uploads"));
 
-
-mongoConnect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("MONGO connected");
-  })
-  .catch(() => {
-    console.log(error);
-  });
-
 const PORT=process.env.PORT || 5000;
+
+// mongoConnect(process.env.MONGO_URL)
+//   .then(() => {
+//     console.log("MONGO connected");
+//   })
+//   .catch(() => {
+//     console.log(error);
+//   });
+
+//   app.listen(PORT,()=>{
+//       console.log(` App lsitening on Port ${PORT}`);    
+//   })
+
+
+
+async function startServer() {
+  try {
+    await mongoConnect(
+      process.env.MONGO_URI
+    );
+    console.log("MONGO connected");
+
+    app.listen(PORT, () => {
+      console.log("App listening on Port", PORT);
+    });
+  } catch (err) {
+    console.error("Mongo connection failed:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
 
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"))
@@ -47,9 +70,3 @@ app.get("/",async(req,res)=>{
 
 app.use('/user',userRoutes);
 app.use('/blog',blogRoutes);
-
-
-app.listen(PORT,()=>{
-    console.log(` App lsitening on Port ${PORT}`);    
-})
-
