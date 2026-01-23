068d967770b896e991c1bbf03cb8809ddf24a804
Yasss333 <yashmandhare3@gmail.com>
2026-01-23 19:04:49 +0530

    Enhance blog interaction features by updating like and save functionality. Implement conditional rendering based on the referer to improve user experience on blog detail and home pages. remov eteh next feature thing

--- Controller/blog.js
+++ Controller/blog.js
@@ -30,10 +30,57 @@ const createBlog = async (req, res) => {
 const incrementLike = async (req, res) => {
   try {
     await Blog.findByIdAndUpdate(req.params.id, { $inc: { likes: 1 } });
-    return res.redirect('back');
+    
+    // Check if request came from blog detail page or home page
+    const referer = req.headers.referer || '';
+    const blogId = req.params.id;
+    
+    if (referer.includes(`/blog/${blogId}`)) {
+      // Rerender blog detail page
+      const Comment = require('../Model/comment.js');
+      const comments = await Comment.find({
+        blogID: blogId,
+      }).populate('createdBY');
+      
+      const blog = await Blog.findById(blogId).populate('createdBY');
+      
+      if (!blog) {
+        return res.redirect('/');
+      }
+      
+      let isSaved = false;
+      if (req.user) {
+        const user = await User.findById(req.user._id);
+        isSaved = user?.savedBlogs?.some(
+          (id) => id.toString() === blog._id.toString()
+        ) || false;
+      }
+      
+      return res.render('viewblog', {
+        user: req.user,
+        blog,
+        comments,
+        isSaved,
+      });
+    } else {
+      // Rerender home page
+      const allBlog = await Blog.find({}).populate('createdBY', 'fullname profileImage');
+      
+      let savedBlogIds = [];
+      if (req.user) {
+        const user = await User.findById(req.user._id);
+        savedBlogIds = user?.savedBlogs?.map(id => id.toString()) || [];
+      }
+      
+      return res.render('home', {
+        user: req.user,
+        blog: allBlog,
+        savedBlogIds: savedBlogIds,
+      });
+    }
   } catch (error) {
     console.log("Failed to like:", error.message);
-    return res.redirect('back');
+    return res.redirect('/');
   }
 };
 const saveBlogHandler = async (req, res) => {
@@ -49,23 +96,66 @@ const saveBlogHandler = async (req, res) => {
       return res.status(404).redirect('/');
     }
 
-    // Check if blog is already saved
-    if (user.savedBlogs.includes(blogID)) {
+    // Check if blog is already saved (compare as strings)
+    const isAlreadySaved = user.savedBlogs.some(
+      (id) => id.toString() === blogID
+    );
+    
+    if (isAlreadySaved) {
       // Unsave: remove from saved blogs
       user.savedBlogs = user.savedBlogs.filter(
         (id) => id.toString() !== blogID
       );
       await user.save();
-      return res.redirect('back');
+    } else {
+      // Save: add to saved blogs
+      user.savedBlogs.push(blogID);
+      await user.save();
     }
 
-    // Save: add to saved blogs
-    user.savedBlogs.push(blogID);
-    await user.save();
-    return res.redirect('back');
+    // Check if request came from blog detail page or home page
+    const referer = req.headers.referer || '';
+    
+    if (referer.includes(`/blog/${blogID}`)) {
+      // Rerender blog detail page
+      const Comment = require('../Model/comment.js');
+      const comments = await Comment.find({
+        blogID: blogID,
+      }).populate('createdBY');
+      
+      const blog = await Blog.findById(blogID).populate('createdBY');
+      
+      if (!blog) {
+        return res.redirect('/');
+      }
+      
+      const updatedUser = await User.findById(req.user._id);
+      const isSaved = updatedUser?.savedBlogs?.some(
+        (id) => id.toString() === blog._id.toString()
+      ) || false;
+      
+      return res.render('viewblog', {
+        user: req.user,
+        blog,
+        comments,
+        isSaved,
+      });
+    } else {
+      // Rerender home page
+      const allBlog = await Blog.find({}).populate('createdBY', 'fullname profileImage');
+      
+      const updatedUser = await User.findById(req.user._id);
+      const savedBlogIds = updatedUser?.savedBlogs?.map(id => id.toString()) || [];
+      
+      return res.render('home', {
+        user: req.user,
+        blog: allBlog,
+        savedBlogIds: savedBlogIds,
+      });
+    }
   } catch (error) {
     console.log("Failed to save:", error.message);
-    return res.redirect('back');
+    return res.redirect('/');
   }
 };
 
